14d5a35b91c4cfdcb52ec4ba7697d119430e0e45
Murtaza Usmani <murtazausmani985@gmail.com>
2022-08-07 00:07:17 +0500

    Sidebar Completed

---
+++ assets/template/sidebar.php
@@ -0,0 +1,57 @@
+<!doctype html>
+<html lang="en">
+  <head>
+  	<title>Sidebar 05</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+		<link rel="stylesheet" href="css/style.css">
+        <script src="https://kit.fontawesome.com/e1a5a5ef59.js" crossorigin="anonymous"></script>
+
+  </head>
+  <body>
+		
+		<div class="wrapper d-flex align-items-stretch vh-100">
+			<nav class="h-100" id="sidebar">
+				<div class="custom-menu">
+					<button type="button" id="sidebarCollapse" class="btn btn-primary">
+	          <i class="fa fa-bars"></i>
+	          <span class="sr-only">Toggle Menu</span>
+	        </button>
+        </div>
+				<div class="p-4 ">
+		  		<h1><a href="./index.php" class="logo">JustEntry </a></h1>
+	        <ul class="list-unstyled components mb-5 mt-5">
+	          <li class="active">
+	            <a href="./index.php" class="mt-3"><span class="fa fa-home mr-3"></span>DashBoard</a>
+	          </li>
+	          <li>
+	              <a href="./upload.php" class="mt-3"><span class="fa fa-solid fa-upload mr-3"></span> Upload</a>
+	          </li>
+	          <li>
+              <a href="" class="mt-3"><span class="fa fa-solid fa-arrow-right-from-bracket mr-3"></span>LogOut</a>
+	          </li>
+	        </ul>
+
+            <div class="footer">
+                    <p>
+                        <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+                        Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserveThis template is made <i class="icon-heart" aria-hidden="true"></i> by <a href="https://colorcom" target="_blank">Colorlib.com</a>
+                        <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></p>
+            </div>
+	       
+
+	      </div>
+    	</nav>
+
+        <!-- Page Content  -->
+
+		</div>
+
+        <!-- SCRIPTS -->
+        <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>    
+        <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+    <script src="js/main.js"></script>
