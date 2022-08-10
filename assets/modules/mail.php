e6596eddbfd24de39bc8e2328b4941224773cdf9
Murtaza Usmani <murtazausmani985@gmail.com>
2022-08-10 22:28:03 +0500

    Mail Functionality In XAMPP

--- assets/modules/mail.php
+++ assets/modules/mail.php
@@ -1,11 +1,11 @@
 <?php
-// $receiver = "murtazausmani2006@gmail.com";
-// $subject = "Email Test via PHP using Localhost";
-// $body = "Hi, there...This is a test email send from Localhost.";
-// $sender = "From:murtazausmani985@gmail.com";
-// if(mail($receiver, $subject, $body, $sender)){
-//     echo "Email sent successfully to $receiver";
-// }else{
-//     echo "Sorry, failed while sending mail!";
-// }
+$to_email = "murtazausmani2006@gmail.com";
+$subject = "Email Test via PHP using Localhost";
+$body = "Hi, there...This is a test email send from Localhost.";
+$headers = "From: murtazausmani985@gmail.com";
+if(mail($receiver, $subject, $body, $sender)){
+    echo "Email sent successfully to $receiver";
+}else{
+    echo "Sorry, failed while sending mail!";
+}
 ?>
