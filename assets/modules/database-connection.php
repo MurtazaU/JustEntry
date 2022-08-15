ea6dd36e38b1255e975aedd91c35c44c150b5faa
Murtaza Usmani <murtazausmani985@gmail.com>
2022-08-15 21:54:42 +0500

    Database Credentials Added

--- assets/modules/database-connection.php
+++ assets/modules/database-connection.php
@@ -1,10 +1,10 @@
 <?php
 
 // 4 Variables
-$dbname = 'justentry';
-$host = "localhost";
-$username = "root";
-$password = "";
+$dbname = 'dbs8219573';
+$host = "db5009695496.hosting-data.io";
+$username = "dbu2803817";
+$password = "h#37K_Vrk&!37";
 
 try {
   $con = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
